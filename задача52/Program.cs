234cb52f334f22ce36a7a72efb1771758389585d
Lena <imagine.youeself19@gmail.com>
2022-07-15 14:57:33 +0300

    добавлена часть решения задачи 52

---
+++ задача52/Program.cs
@@ -0,0 +1,34 @@
+﻿int[, ] CreateMatr(int n, int m)
+{
+int[, ] matrix = new int[n ,m];
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+matrix[i, j] = new Random().Next(1, 10);
+Console.Write(matrix[i, j] + " ");
+}
+Console.WriteLine();
+}
+return matrix;
+}
+
+
+void PrintMatr(int[, ] matrix)
+{
+Console.WriteLine();
+double SumOfColumnElements = 0;
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+SumOfColumnElements += matrix[i, j];
+}
+}
+Console.Write($"{ SumOfColumnElements / matrix.GetLength(0)} ");
+}
+Console.WriteLine("Введите кол-во строк в матрице:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите кол-во столбцов в матрице:");
+int m = Convert.ToInt32(Console.ReadLine());
+PrintMatr(CreateMatr(n, m));
