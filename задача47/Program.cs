1e257cd1b7b21f569c0565b368d1a2c9894a0123
Lena <imagine.youeself19@gmail.com>
2022-07-15 13:27:58 +0300

    реешна задача47

---
+++ задача47/Program.cs
@@ -0,0 +1,29 @@
+﻿double[, ] CreateMatr(int n, int m)
+{
+double[, ] matrix = new double[n ,m];
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+Random rand = new Random();
+matrix[i, j] = rand.NextDouble();
+matrix[i, j] = Math.Round(matrix[i, j], 2);
+}
+}
+return matrix;
+}
+
+
+void PrintMatr(double[, ] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+Console.Write(matrix[i, j] + " ");
+}
+Console.WriteLine();
+}
+}
+
+PrintMatr(CreateMatr(3, 4));
